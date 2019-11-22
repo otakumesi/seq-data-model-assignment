f622bfff40cb33abf0d6d585e823ac0021be4b53
Takuro Niitsuma <takuro-n@h28is016.naist.jp>
2019-11-22 22:18:18 +0900

    np.eye -> eye & format周りを平坦にした

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import eye
 from numpy.linalg import inv
 
 
@@ -12,40 +13,32 @@ class FactorAnalysis:
         X, Z, Z_by_Z, X_by_X, X_by_Z = self._perform_expectation(X)
         print("problem 1 answer: updated mu = {}, X = {}".format(Z, X))
         print("problem 2 answer: Σ^(Z|X) = {}, <z>_n = {}, <zz>_n = {}"
-              .format(self._calc_sigma_of_Z(),
-                      self._calc_squared_latent_mu(Z),
-                      Z_by_Z))
+              .format(self._calc_sigma_of_Z(), self._calc_squared_latent_mu(Z), Z_by_Z))
 
         n_samples = X.shape[0]
-        print("problem 3 answer: N = {}, <x'x'^T> = {} "
-              "<zz^T> ={}, <x'<z>^T> = {}"
+        print("problem 3 answer: N = {}, <x'x'^T> = {} <zz^T> ={}, <x'<z>^T> = {}"
               .format(n_samples, X_by_X, Z_by_Z, X_by_Z))
 
-        updated_W, update_cov_mat = self._perform_maximization(n_samples,
-                                                           Z_by_Z,
-                                                           X_by_X,
-                                                           X_by_Z)
-        print("problem 4 answer: W = {}, Σ = {}"
-              .format(updated_W, update_cov_mat))
-        print("problem 5 answer: after = {}, before = {}".format(
-              self.sigma, update_cov_mat))
+        updated_W, update_cov_mat = self._perform_maximization(n_samples, Z_by_Z, X_by_X, X_by_Z)
+        print("problem 4 answer: W = {}, Σ = {}".format(updated_W, update_cov_mat))
+        print("problem 5 answer: after = {}, before = {}".format(self.sigma, update_cov_mat))
 
     def _perform_expectation(self, X):
         mean_vec = np.mean(X)
         X = X - mean_vec
         Z = self._calc_sigma_of_Z() @ self.W.T @ inv(self.sigma) @ X.T
-        X_by_X = X.T @ X * np.eye(2)
+        X_by_X = X.T @ X * eye(2)
         Z_by_Z = Z @ Z.T + self._calc_sigma_of_Z()
         X_by_Z = X.T @ Z.T
         return X, Z, X_by_X, Z_by_Z, X_by_Z
 
     def _perform_maximization(self, n_samples, Z_by_Z, X_by_X, X_by_Z):
         updated_W = (X_by_Z.T @ inv(Z_by_Z)).T
-        updated_cov_mat = (X_by_X - X_by_Z @ updated_W.T) * np.eye(2) / n_samples
+        updated_cov_mat = (X_by_X - X_by_Z @ updated_W.T) * eye(2) / n_samples
         return updated_W, updated_cov_mat
 
     def _calc_sigma_of_Z(self):
-        return inv(self.W.T @ inv(self.sigma) @ self.W + np.eye(1))
+        return inv(self.W.T @ inv(self.sigma) @ self.W + eye(1))
 
     def _calc_squared_latent_mu(self, Z):
         return np.multiply(Z, Z) + self._calc_sigma_of_Z()
